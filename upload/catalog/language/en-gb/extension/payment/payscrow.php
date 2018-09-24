bc51d6f92f2db9309aeb08edcab48797d5188d2e
Chibuzor Ogbu <chibuzorogbu@gmail.com>
2018-09-24 17:38:32 +0100

    minor fixes

---
+++ upload/catalog/language/en-gb/extension/payment/payscrow.php
@@ -0,0 +1,40 @@
+<?php
+// Heading
+$_['text_title']				= '<img src="http://payscrow.net/assets/logos/logo-black.png" alt="Payscrow" title="Payscrow" style="border: 1px solid #EEEEEE;" />';
+
+// Button
+$_['button_confirm']			= 'Continue';
+
+// Text
+$_['text_new_card']				= 'New card';
+$_['text_address_response']		= 'Address verification: ';
+$_['text_address_ppx']			= 'No address data provided or Address not checked by the Card Issuer';
+$_['text_address_yyy']			= 'Card Issuer confirmed that street and postcode match with their records';
+$_['text_address_yna']			= 'Card Issuer confirmed that street matches with their records but postcode does not match';
+$_['text_address_nyz']			= 'Card Issuer confirmed that postcode matches with their records but street does not match';
+$_['text_address_nnn']			= 'Both street and postcode do not match with the Card Issuer records';
+$_['text_address_ypx']			= 'Card Issuer confirmed that street matches with their records. The Issuer did not check the postcode';
+$_['text_address_pyx']			= 'Card Issuer confirmed that postcode matches with their records. The Issuer did not check the street';
+$_['text_address_xxu']			= 'Card Issuer did not check the AVS information';
+$_['text_card_code_verify']		= 'Security code: ';
+$_['text_card_code_m']			= 'Card security code match';
+$_['text_card_code_n']			= 'Card security code does not match';
+$_['text_card_code_p']			= 'Not processed';
+$_['text_card_code_s']			= 'Merchant has indicated that the card security code is not present on the card';
+$_['text_card_code_u']			= 'Issuer is not certified and/or has not provided encryption keys';
+$_['text_card_code_x']			= 'No response from the credit card association was received';
+$_['text_card_code_blank']		= 'A blank response should indicate that no code was sent and that there was no indication that the code was not present on the card.';
+$_['text_card_type_m']			= 'Mastercard';
+$_['text_card_type_v']			= 'Visa';
+$_['text_card_type_i']			= 'Verve';
+$_['text_response_code_full']	= 'Approval code: ';
+$_['text_response_code']		= 'Full response code: ';
+$_['text_response_card']		= 'Card used: ';
+$_['text_response_card_type']	= 'Card type: ';
+$_['text_response_proc_code']	= 'Processor code: ';
+$_['text_response_ref']			= 'Ref number: ';
+
+// Error
+$_['error_failed']				= 'Unable to process your payment, please try again';
+//success
+$_['success']				= 'Your payment was successful';
